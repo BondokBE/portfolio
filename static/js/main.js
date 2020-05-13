3841b6350b5c86db4c3e5d37fdc2ca908dd12802
unknown <belalabdulnasser@gmail.com>
2020-05-13 17:40:04 +0200

    Added toggle dark theme button at the right  nav bar, and made it toggling light/dark themes + the Jquery code of it

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,9 @@
 
+// Smooth scroll
+
 $(document).ready(function(){
   
-  $('.nav .links a').on('click', function(event) {
+  $('a').on('click', function(event) {
     
     var hash = this.hash;
     $('html, body').animate({
@@ -12,3 +14,28 @@ $(document).ready(function(){
     
   }); 
 });
+
+// Toggle light and dark themes
+
+$(document).ready(function () {
+
+  "use strict";
+
+  $(".nav .switch i").click(function () {
+
+      //? worked with hassClass not attr
+
+      if ($(".nav .switch i").hasClass('fa-toggle-off')) {
+        $("link[href='static/css/light_theme.css']").replaceWith('<link rel="stylesheet" href="static/css/dark_theme.css">');
+        $(".nav .switch i").removeClass('fa fa-toggle-off fa-lg'); // change the attr value
+        $(".nav .switch i").addClass('fa fa-toggle-on fa-lg'); // change the attr value
+      } else if ($(".nav .switch i").hasClass('fa-toggle-on')) {
+        $("link[href='static/css/dark_theme.css']").replaceWith('<link rel="stylesheet" href="static/css/light_theme.css">');
+        $(".nav .switch i").removeClass('fa fa-toggle-on fa-lg'); // change the attr value
+        $(".nav .switch i").addClass('fa fa-toggle-off fa-lg'); 
+      }
+
+    });
+
+});
+
